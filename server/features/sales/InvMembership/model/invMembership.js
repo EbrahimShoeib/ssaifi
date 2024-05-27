c3385e9455c48e7945b672f1421d41234bb798b3
ibrahimshoeib <hsho2214@gmail.com>
2024-05-27 23:47:02 +0300

    enums

--- server/features/sales/InvMembership/model/invMembership.js
+++ server/features/sales/InvMembership/model/invMembership.js
@@ -20,7 +20,7 @@ endDate:{type:String,
     require:true},
 status:{
     type:String,
-    enum :["active","inactive"] ,
+    enum :["active","expired"] ,
     require:true
 }
 })
@@ -34,7 +34,7 @@ function createNewInvMembership(obj){
         membershipType:joi.string().required().min(1).max(40),
         startDate:joi.string().required().min(1).max(40),
         endDate:joi.string().required().min(1).max(40),
-        status:joi.string().required().valid("active","inactive").min(1).max(40)
+        status:joi.string().required().valid("active","expired").min(1).max(40)
 
     })
     return schema.validate(obj)
@@ -46,7 +46,7 @@ function updateInvMembership(obj){
         membershipType:joi.string().min(1).max(40),
         startDate:joi.string().min(1).max(40),
         endDate:joi.string().min(1).max(40),
-        status:joi.string().valid("active","inactive").min(1).max(40)
+        status:joi.string().valid("active","expired").min(1).max(40)
 
     })
     return schema.validate(obj)
