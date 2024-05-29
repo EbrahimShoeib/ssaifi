93facc3857dbce0bf12f162f60ef1773cae1c6b3
ibrahimshoeib <hsho2214@gmail.com>
2024-05-29 18:36:38 +0300

    chore

--- server/core/utils/upload.js
+++ server/core/utils/upload.js
@@ -2,15 +2,26 @@ const path = require("path")
 
 const multer = require("multer")
 
+// const storage = multer.diskStorage({
+//     destination : function(req,file,cd) {
+//         cd(null,"uploads/")
+//     },
+//     filename : function(req,file,cd){
+//         cb(null, `${Date.now()}-${file.originalname}`); // Generate a unique filename
+//     }
+// })
+
+// const upload = multer({storage : storage }) 
+// Set up Multer storage configuration
 const storage = multer.diskStorage({
-    destination : function(req,file,cd) {
-        cd(null,"uploads/")
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/'); // Specify the upload directory
     },
-    filename : function(req,file,cd){
-        cd(null,Date.now()+ path.extname(file.originalname))
-    }
-})
-
-const upload = multer({storage : storage }) 
-
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`); // Generate a unique filename
+    },
+  });
+  
+  // Initialize Multer with the storage configuration
+  const upload = multer({ storage });
 module.exports = upload
