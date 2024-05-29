97429e24b1771c362a6df7eff7b6e9533b548c0c
ibrahimshoeib <hsho2214@gmail.com>
2024-05-29 18:20:56 +0300

    chore

--- server/core/utils/upload.js
+++ server/core/utils/upload.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
         cd(null,"uploads/")
     },
     filename : function(req,file,cd){
-        cd(null,Date.now()+ path.extname(file.originalname))
+        cb(null, `${Date.now()}-${file.originalname}`); // Generate a unique filename
     }
 })
 
